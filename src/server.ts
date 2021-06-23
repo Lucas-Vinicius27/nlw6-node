639b5171d05f77e4fa4a6b028aa5612567a09d32
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-06-23 18:43:41 -0300

    feat: tratativa de erros no server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, response, Response } from "express";
+import "express-async-errors";
 import "./database";
 import { routes } from "./routes";
 
@@ -8,5 +9,15 @@ const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({ messagem: err.message, error: err });
+    }
+
+    return res.status(500).json({
+        message: "Internal Server Error!",
+        status: "Error!"
+    });
+});
 
 app.listen(PORT, () => console.log(`Server is runnig on port ${PORT}!`));
