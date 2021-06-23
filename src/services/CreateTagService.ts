0e339319b69b09a3bb817e358596d74c9585251f
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-06-23 19:29:45 -0300

    feat: criação do service tag

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from "typeorm";
+import { TagsRepositories } from "../repositories/TagsRepositories";
+
+class CreateTagService {
+    async execute(name: string) {
+        const tagRepository = getCustomRepository(TagsRepositories);
+
+        if (!name) {
+            throw new Error("Name incorrect!");
+        }
+
+        const tagAlreadyExists = await tagRepository.findOne({ name });
+
+        if (tagAlreadyExists) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagRepository.create({ name });
+
+        await tagRepository.save(tag);
+
+        return tag;
+    }
+}
+
+export { CreateTagService };
