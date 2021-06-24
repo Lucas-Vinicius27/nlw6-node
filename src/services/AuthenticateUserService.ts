6fb2e53aa162f3ffd86c0b5f40b763f57d9bf073
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-06-23 22:53:08 -0300

    feat: criação do service de autenticação

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,37 @@
+import { compare } from "bcryptjs";
+import { getCustomRepository } from "typeorm";
+import { sign } from "jsonwebtoken";
+import { UserRepositories } from "../repositories/UserRepositories";
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const userRepository = getCustomRepository(UserRepositories);
+        const user = await userRepository.findOne({ email });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect!");
+        }
+
+        const token = sign({
+            email: user.email
+        }, "9bbe2d4423a31ba73be864b45b80a367", {
+            subject: user.id,
+            expiresIn: "1h"
+        });
+
+        return token;
+    }
+}
+
+export { AuthenticateUserService };
