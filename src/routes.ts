ff96df68a0500682ef4f8b34dec12b14f1df8919
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-06-23 22:52:03 -0300

    feat: criação da rota login

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
@@ -6,8 +7,10 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 const routes = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 routes.post("/users", createUserController.handle);
 routes.post("/tags", ensureAdmin, createTagController.handle);
+routes.post("/login", authenticateUserController.handle);
 
 export { routes };
