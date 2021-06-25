e20c4ec46471462017b7b12c8412b36344b5f345
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-06-24 22:36:38 -0300

    feat: pegando id do usuário que vem do midd

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,12 +3,13 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class CreateComplimentController {
     async handle(req: Request, res: Response) {
-        const { tag_id, user_sender, user_receiver, message } = req.body;
+        const { tag_id, user_receiver, message } = req.body;
+        const { users_id } = req;
         const createComplimentService = new CreateComplimentService();
         const compliment = await createComplimentService.execute({
             tag_id,
             user_receiver,
-            user_sender,
+            user_sender: users_id,
             message
         });
         return res.json(compliment);
