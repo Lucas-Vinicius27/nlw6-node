6bb7c082ed478250499125ca6eb3e80afaa08182
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-06-24 22:28:37 -0300

    feat: validando se o usuário é admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,11 @@
 import { Response, Request, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+    const { users_id } = req;
+    const userRepository = getCustomRepository(UsersRepositories);
+    const { admin } = await userRepository.findOne(users_id);
 
     if (admin) {
         return next();
